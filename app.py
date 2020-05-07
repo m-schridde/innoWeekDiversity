9f2d7459b7021858ff3cffc4703cbd498da6098e
Selina Weis <selina.weis@sap.com>
2020-05-07 15:00:22 +0200

    statistic html & about html added

--- app.py
+++ app.py
@@ -14,5 +14,13 @@ def index():
 def posts():
     return render_template('posts.html')
 
+@app.route('/statistics.html')
+def statistics():
+    return render_template('statistics.html')
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
